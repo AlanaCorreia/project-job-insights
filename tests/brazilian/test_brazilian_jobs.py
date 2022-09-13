f112ad4ccbdf253d7a297a577bb57c8445bf49c2
Alana Correia <alanacorreia.ac@gmail.com>
2022-09-13 11:39:33 -0300

    feat: implementa teste da função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs_list = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    expectedKeys = ["title", "salary", "type"]
+
+    for job in jobs_list:
+        for key in expectedKeys:
+            assert key in job.keys()
