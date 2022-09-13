c0982b6c887cc3c098cdd9cb9614dce7059c98f1
Alana Correia <alanacorreia.ac@gmail.com>
2022-09-13 10:50:04 -0300

    feat: implementa teste da função counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result_javascript = count_ocurrences("src/jobs.csv", "Javascript")
+    assert result_javascript == 122
+
+    result_python = count_ocurrences("src/jobs.csv", "Python")
+    assert result_python == 1639
